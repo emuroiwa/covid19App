ae8dd1b97e5dec8abc1e42cdb7d6894d6af57f21
Ernest <emuroiwa@gmail.com>
2020-08-02 21:21:34 +0200

    master : wip api data not rendering on initState

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,4 +1,8 @@
+import 'package:covid19_zim/app/repositories/data_repository.dart';
+import 'package:covid19_zim/app/services/api.dart';
+import 'package:covid19_zim/ui/endpoint_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -6,9 +10,34 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _cases;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateData();
+  }
+
+  Future<void> _updateData() async {
+    final dataRepository = Provider.of<DataRepository>(context);
+    final cases = await dataRepository.getEndpointData(EndPoint.cases);
+    setState(() {
+      _cases = cases;
+      print(_cases);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Covid19 Tracker')), body: ListView());
+        appBar: AppBar(title: Text('Covid19 Tracker')),
+        body: ListView(
+          children: <Widget>[
+            EndpointCard(
+              endPoint: EndPoint.cases,
+              value: _cases,
+            )
+          ],
+        ));
   }
 }
